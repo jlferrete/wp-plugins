9fffcb92d42fd394ceca1a6c76038fb92f8ce821
Jlferrete <joseluisferrete@gmail.com>
2019-06-20 18:33:07 +0200

    Improved name of functions

--- quizbook/includes/posttypes.php
+++ quizbook/includes/posttypes.php
@@ -1,6 +1,6 @@
 <?php
 
-function post_type() {
+function quizbook_post_type() {
     $labels = array(
         'name'                  => _x( 'Quiz', 'Post type general name', 'quizbook' ),
         'singular_name'         => _x( 'Quiz', 'Post type singular name', 'quizbook' ),
@@ -47,13 +47,13 @@ function post_type() {
     register_post_type( 'Quizzes', $args );
 }
 
-add_action( 'init', 'post_type' );
+add_action( 'init', 'quizbook_post_type' );
 
 /*
 **  Flush Rewrite
 */
 
-function rewrite_flush() {
-  post_type();
+function quizbook_rewrite_flush() {
+  quizbook_post_type();
   flush_rewrite_rules();
 }
--- quizbook/quizbook.php
+++ quizbook/quizbook.php
@@ -19,4 +19,4 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/posttypes.php';
 /*
 *   Regenera las reglas de las URL al activar
 */
-register_activation_hook( __FILE__, 'rewrite_flush' );
+register_activation_hook( __FILE__, 'quizbook_rewrite_flush' );
