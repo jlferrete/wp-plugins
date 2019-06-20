fd9212229fdada0313493cec80ce088efe3586a7
Jlferrete <joseluisferrete@gmail.com>
2019-06-20 12:42:58 +0200

    Initial installation

---
+++ quizbook/quizbook.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Quizbook
+Version: 0.1
+Plugin URI:
+Description: Plugin para añadir quizzes o cuestionarios con Wordpress
+Author: Jlferrete
+Author URI:
+License: GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: quizbook
+*/
