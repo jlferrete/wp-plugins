083da5b21edff421315bb956d235657a4aaadf19
JlFerrete <joseluisferrete@gmail.com>
2019-08-18 23:48:10 +0200

    Problem in Shortcode WP_Query

--- quizbook/includes/shortcode.php
+++ quizbook/includes/shortcode.php
@@ -7,6 +7,29 @@ if (! defined('ABSPATH')) exit();
 */
 
 function quizbook_shortcode( ) {
-    echo "Desde shortcode";
+	
+	$args = array(
+		'post_type' => 'quizes',
+	);
+	$quizbook = new WP_Query($args); ?>
+	<form name="quizbook_enviar" id="quizbook_enviar">
+		<div id="quizbook" class="quizbook">
+			<ul>
+			<?php while($quizbook->have_posts()): $quizbook->the_post(); ?>
+				<li data-pregunta="<?php echo get_the_ID(); ?>">
+					<h3><?php the_title(); ?></h3>
+					<?php the_content(); ?>
+				</li>
+	
+			<?php endwhile; ?>
+			</ul>
+		</div> <!--#quizbook-->
+	
+		<input type="submit" value="Enviar" id="quizbook_btn_submit">
+	
+		<div id="quizbook_resultado"></div>
+	</form><!--form-->
+	<?php wp_reset_postdata();
+	    
 }
 add_shortcode( 'quizbook', 'quizbook_shortcode' );
